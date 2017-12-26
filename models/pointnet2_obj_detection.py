d86dd09da9aa8fd913dd2443f9550e752ed76ffc
Benzlxs <xuesong.li@unsw.edu.au>
2017-12-27 00:15:46 +1100

    add classification

--- models/pointnet2_obj_detection.py
+++ models/pointnet2_obj_detection.py
@@ -15,10 +15,13 @@ BASE_DIR = os.path.dirname(__file__)
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, '../utils'))
 sys.path.append(os.path.join(BASE_DIR, '../config'))
+sys.path.append(os.path.join(BASE_DIR, '../utils_xyz'))
 import tensorflow as tf
 import numpy as np
 from config import cfg
+import numpy.random as npr
 import tf_util
+from 3D_box_transform import 3D_box_transform
 from pointnet_util import pointnet_sa_module, pointnet_fp_module
 from sklearn.metrics.pairwise import euclidean_distances
 
@@ -74,11 +77,11 @@ def get_model(point_cloud, is_training, num_class, bn_decay=None):
     return end_points, net_class, net_boxes, l4_xyz
 
 
-def get_loss(pred_class, pred_box, label, smpw, xyz):
+def get_loss(pred_class, pred_box, gt_box, smpw, xyz):
     """
     Input: pred_class: BxNx3xC
            pred_box: BxNx3x7
-           label: BxNx7
+           gt_box: BxNx7
 	       smpw: BxN
     Description: similar to faster rcnn, overlaps between prediction boxes and ground truth boxes are estimated to decide point labels and further calucuate the box_targets
     --> preparing all anchors in every space point --> calculating overlaps bewteen anchor boxes and bounding boxes -->  anchors whose overlaps are over a certain threshold are regarded as positive labels, the rest are negative labels
@@ -104,17 +107,19 @@ def get_loss(pred_class, pred_box, label, smpw, xyz):
 
     for n in range(0,NUM_BATCH)
         # N is the points number of xyz
-        # all_alpha is the [N 2] 2 anchors
+        # all_alpha is the [N A] A anchors, reshape to [N*A 1]
         # dif_alpha is the angle substract with label
         # calculate the angle gap to select one anchor
         N =  xyz[n].shape[1]
         all_alpha = np.tile(cfg.TRAIN.Alpha, (N,1))
-        dif_alpha = all_alpha - label[n].alpha
-
+        dif_alpha = all_alpha - gt_box[n].alpha
+	dif_alpha = dif_alpha.reshape(-1,1)
         # estimate the central points distance
-        distance = euclidean_distances(xyz[n],label[n].xyz)
+        distance = euclidean_distances(xyz[n], gt_box[n,:,1:4])
+	distance = np.tile(distance, (1,A))
+ 	distance = distance.reshape(-1,  label[n].shape(0)) # label[n].shape(0)	is the number of ground truth
 
-        labels = np.zeros(shape=(N, A))
+        labels = np.zeros(shape=(N*A,1))
         labels.fill(-1)
         # decide positive and negative labels
 	argmin_dist = distance.argmax(axis=1)
@@ -123,23 +128,50 @@ def get_loss(pred_class, pred_box, label, smpw, xyz):
 	gt_min_dist  = distance[gt_argmin_dist, np.arange(distance.shape[1])]
 	gt_argmin_dist = np.where(distance == gt_min_dist)[0]
 	
+	# Deleting hard coding
+	# arg_alpha0  = np.where(np.absolute(dif_alpha[:,0]) <  np.pi/4)  # alpha is 0
+	# arg_alpha90 = np.where(np.absolute(dif_alpha[:,1]) <= np.pi/4) # alpha is 90
+	# labels[np.intersect1d(arg_alpha0,gt_argmin_dist),0]  = 1 # alpha 0
+	# labels[np.intersect1d(arg_alpha90,gt_argmin_dist),1] = 1 # alpha 90
+	# labels[np.intersect1d(arg_alpha0 ,(min_dist < cfg.TRAIN.POSITIVE_CEN_DIST)),0] = 1
+	# labels[np.intersect1d(arg_alpha90,(min_dist < cfg.TRAIN.POSITIVE_CEN_DIST)),1] = 1	
+	#labels[np.intersect1d(arg_alpha90 ,(min_dist > cfg.TRAIN.NEGATIVE_CEN_DIST)),0] = 0
+	#labels[np.intersect1d(arg_alpha0, (min_dist > cfg.TRAIN.NEGATIVE_CEN_DIST)),1] = 0
+	arg_alpha = np.where(np.absolute(dif_alpha[:0] < cfg.TRAIN.POSITIVE_ALPHA))[0]
+	labels[np.intersect1d(arg_alpha, gt_argmin_dist)] = 1
+	min_dist_positive_inds = np.where(min_dist < cfg.TRAIN.POSITIVE_CEN_DIST)[0]
+	labels[np.intersect1d(arg_alpha, min_dist_positive_inds)] = 1
+	min_dist_negative_inds = np.where(min_dist > cfg.TRAIN.NEGATIVE_CEN_DIST)[0]
+	labels[np.intersect1d(arg_alpha, min_dist_negative_inds)] = 0
 
-	arg_alpha0  = np.where(np.absolute(dif_alpha[:,0])< np.pi/4)  # alpha is 0
-	arg_alpha90 = np.where(np.absolute(dif_alpha[:,1])<= np.pi/4) # alpha is 90
-	
-	labels[np.intersect1d(arg_alpha0,gt_argmin_dist),0]  = 1 # alpha 0
-	labels[np.intersect1d(arg_alpha90,gt_argmin_dist),1] = 1 # alpha 90
-	
-	labels[np.intersect1d(arg_alpha0 ,(min_dist<cfg.POSITIVE_CEN_DIST)),0] = 1
-	labels[np.intersect1d(arg_alpha90,(min_dist<cfg.POSITIVE_CEN_DIST)),1] = 1
+	num_positive_labels = int( cfg.TRAIN.RPN_FG_FRACTION*cfg.TRAIN.RPN_BATCHSIZE )
+	# labels = labels.reshape(-1,1)
+	positive_inds = np.where(labels == 1)[0]
+	if len(positive_inds) > num_positive_labels:
+		disable_inds = npr.choice(
+			positive_inds, size = (len(positive_inds) - num_positive_labels), replace = False )
+		labels[disable_inds] = -1
 
-	num_
+	num_negative_labels = cfg.TRAIN.RPN_BATCHSIZE - np.sum(labels == 1)
+	negative_inds = np.where(labels == 0)[0]
+	if len(negative_inds) > num_negative_labels
+		disable_inds = npr.choice(
+			negative_inds, size = (len(negative_inds) - num_negative_labels), replace = False )
+		labels[disable_inds] = -1
+	# labels = labels.reshape(-1,A)
+	box_targets = np.zeros((N*A,cfg.TRAIN.NUM_REGRESSION, dtype = np.float32 )
+	box_targets = 3D_box_transform()
 
         # calculate the box targets between anchors and ground truth
-
+	
         # outside weights and inside_weigths
 
         # classification loss
+	pred_class = tf.reshape(pred_class, [-1,2])
+	labels = tf.reshape(labels, [-1])
+	pred_class = tf.reshape(tf.gather(pred_class, tf.where(tf.not_equal(labels,-1))),[-1, 2])
+	labels = tf.reshape(tf.gather(labels, tf.where(tf.not_equal(labels, -1))), [-1])
+	loss_classification = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = pred_class, labels = labels))
 
         # regression loss
 
