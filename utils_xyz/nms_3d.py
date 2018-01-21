50102e1e1dbe9519d39069fec7e2a4c0b3990a84
Benzlxs <xuesong.li@unsw.edu.au>
2018-01-21 22:57:33 +1100

    1

--- utils_xyz/nms_3d.py
+++ utils_xyz/nms_3d.py
@@ -23,13 +23,13 @@ def nms_3d(all_boxes, thresh = 0.3):
     # initialize the list of picked indices
     pick = []
     ## sortting the scores
-    indices_box = np.argsort(all_boxes[:, 4])
-
+    indices_box = np.argsort( all_boxes[:, 7])
+    coordinate_list = convert_to_list_points(all_boxes) # shape: n x 5, data type is list
     while(len(indices_box)>0):
         last = len(indices_box) - 1
         i = indices_box[last]
         pick.append(i)
-
+        # convert into list of coordinate point of birdview
         ## compare i with rest of boxes
 
 
@@ -46,9 +46,18 @@ def convert_to_list_points(all_boxes):
     all_coordinates = []
     num_point = all_boxes.shape[0]
     for i in range(num_point):
-        temp_coordinate = [[],
-                           [],
-                           [],
-                           [],
-                           [],
-                           []]
+        # after rotation, x = cos(theta)*x0 - sin(theta)*y0
+        # y = sin(theta)*x0 + cos(theta)*y0
+        l = all_boxes[i, 0]
+        w = all_boxes[i, 1]
+        xy = np.array([[-w/2.0, l/2.0],[w/2.0, l/2.0],[w/2.0, -l/2.0],[-w/2.0, -l/2.0], [-w/2.0, l/2.0]])
+        theta = - all_boxes[i, 3]
+        T  = np.array([[np.cos(theta), np.sin(theta) ],[-np.sin(theta), np.cos(theta)]])
+        a_xy = np.dot(xy, T)
+        x_ctr = all_boxes[i, 4]
+        y_ctr = all_boxes[i, 5]
+        temp_coordinate = a_xy + np.array([[x_ctr, y_ctr]])
+        temp_coordinate = temp_coordinate.tolist()  # shape: 5 x 2, list
+        all_coordinates.append(temp_coordinate)
+
+    return all_coordinates
