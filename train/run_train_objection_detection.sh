9bf9c3fb5b0f93050b71b260211bb32f9e3e0458
Benzlxs <xuesong.li@unsw.edu.au>
2018-02-02 09:50:31 +1100

    sh

--- train/run_train_objection_detection.sh
+++ train/run_train_objection_detection.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 
-train_script = train_obj_detection.py
-dataset_name = KITTI
-baselogname = log
-maxepoch = 30
-batchsize = 20
-numpoint = 2**15
-feed_label_elements = "label_category"
+train_script=train_obj_detection.py
+dataset_name=KITTI
+baselogname=log
+maxepoch=30
+batchsize=20
+numpoint=2**15
+feed_label_elements="label_category"
 
-debug_code = "pudb $train_script --dataset_name $dataset_name --log_dir $baselogname --max_epoch $maxepoch --batch_size $batchsize --num_point $numpoint"
+debug_code="pudb $train_script --dataset_name $dataset_name --log_dir $baselogname --max_epoch $maxepoch --batch_size $batchsize --num_point $numpoint"
 
-run_code = "ipython $train_script --dataset_name $dataset_name --log_dir $baselogname --max_epoch $maxepoch --batch_size $batchsize --num_point $numpoint"
+run_code="ipython $train_script --dataset_name $dataset_name --log_dir $baselogname --max_epoch $maxepoch --batch_size $batchsize --num_point $numpoint"
 
 
 
