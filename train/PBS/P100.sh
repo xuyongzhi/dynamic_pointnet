52ddbef99aea0a84d14375f080d809f32ac2dfdb
xuyongzhi <buaaxyz@yeah.net>
2018-04-13 14:02:41 +1000

    update P100.sh

--- train/PBS/P100.sh
+++ train/PBS/P100.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -q gpupascal
-#PBS -l walltime=36:00:00
-#PBS -l mem=46GB
+#PBS -l walltime=12:00:00
+#PBS -l mem=26GB
 #PBS -l jobfs=0GB
-#PBS -l ngpus=4
-#PBS -l ncpus=24
+#PBS -l ngpus=2
+#PBS -l ncpus=12
 ## For licensed software, you have to specify it to get the job running. For unlicensed software, you should also specify it to help us analyse the software usage on our system.
 #PBS -l software=tensorflow/1.6-cudnn7.1-python2.7
 
@@ -17,12 +17,8 @@
 module load  tensorflow/1.6-cudnn7.1-python2.7
 module list
  
-#feed_data_elements='xyz_midnorm_block-color_1norm' 
+feed_data_elements='xyz_midnorm_block-color_1norm' 
 #feed_data_elements='xyz_midnorm_block-color_1norm-nxnynz' 
-feed_data_elements='xyz_midnorm_block' 
-
-./run_train_seg_presg_mat.sh 4aG_114 90 0 $feed_data_elements E 0.3   -> out_scan114_E.log &
-./run_train_seg_presg_mat.sh 4aG_114 90 0 $feed_data_elements CN 0.3   -> out_scan114_CN.log &
-./run_train_seg_presg_mat.sh 4aG_114 90 0 $feed_data_elements CN 1.3   -> out_scan114_nodrop.log &
-./run_train_seg_presg_mat.sh 4aG_144 80 0 $feed_data_elements CN 0.3   -> out_scan144.log 
-
+#feed_data_elements='xyz_midnorm_block' 
+#./train_seg_presg_scan.sh 4bG_114 24 2 $feed_data_elements N 1.3 -> out_4bG114.log
+./train_seg_presg_scan.sh 4aG_114 18 2 $feed_data_elements N 1.3 -> out_4aG114.log
