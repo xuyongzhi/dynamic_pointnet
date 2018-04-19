a423e206ee7bc9a7c6bc90975a080d253cd90961
benzlxs <xuesong.li@unsw.edu.au>
2018-04-19 23:06:03 +1000

    adding train bird view detection

--- config/config.py
+++ config/config.py
@@ -23,6 +23,7 @@ __C = edict()
 #   from fast_rcnn_config import cfg
 cfg = __C
 
+# DEBUG = False
 #
 # Training options
 #
@@ -33,38 +34,52 @@ __C.TRAIN = edict()
 __C.TRAIN.NUM_ANCHORS = 2
 
 # number of classification
-__C.TRAIN.NUM_CLASS = 2  ## background and vehicle, training from simple situation
+__C.TRAIN.NUM_CLASSES = 2  ## background and vehicle, start training from simple situation
 
-# number of regression parameters,  7 = x,y,z,l,w,h,theta
-__C.TRAIN.NUM_REGRESSION = 7
+# number of channels
+__C.TRAIN.NUM_CHANNELS = 3 ## xyz
+
+# number of evaluation
+__C.TRAIN.EVALUATION_NUM = 700 # 1000
+
+# number of regression parameters,  7 = x,y,l,w, alpha
+__C.TRAIN.NUM_REGRESSION = 5
 
 # radius for grouping
-__C.TRAIN.Radius_1 = 1
-__C.TRAIN.Radius_2 = 1
-__C.TRAIN.Radius_3 = 1
-__C.TRAIN.Radius_4 = 1
-
+__C.TRAIN.Radius_1 = 0.4
+__C.TRAIN.Radius_2 = 0.8
+__C.TRAIN.Radius_3 = 1.6
+__C.TRAIN.Radius_4 = 2.4
+__C.TRAIN.Radius_5 = 3.2
+__C.TRAIN.Radius_6 = 2.6
 
 # 3D anchor size, the same size (l=3.9m, w=1.6m, h=1.7m) but with different
 # orientation alpha (pi = 0, pi/4, pi/2)
 l=3.9
 w=1.6
 h=1.7
-__C.TRAIN.Anchors = np.array([l, w, h])
-__C.TRAIN.Alpha = np.array([0, np.pi/2])  ## if 2 anchor is not enought, change it to 4, [0, np.pi/4, np.pi/2, np.pi*3/4]
+__C.TRAIN.Anchors = np.array([l, w])
+__C.TRAIN.Alpha = np.array([[0], [np.pi/2]])  ## if 2 anchor is not enought, change it to 4, [0, np.pi/4, np.pi/2, np.pi*3/4]
 __C.TRAIN.Anchor_bv = np.array([[l/2,  w/2,  -l/2,  -w/2 ] , [w/2,  l/2,  -w/2,  -l/2]])   ## [frowart_left back_right]
 
+
+## the number of anchor coordinate lists is up to the number of anchors
+__C.TRAIN.Anchor_coordinate_lists = np.array([[[-w/2.0, l/2.0],[w/2.0, l/2.0],[w/2.0, -l/2.0],[-w/2.0, -l/2.0], [-w/2.0, l/2.0]],
+                                             [ [-l/2.0, w/2.0],[l/2.0, w/2.0],[l/2.0, -w/2.0],[-l/2.0, -w/2.0], [-l/2.0, w/2.0] ]])
 # Use horizontal flipper point cloud, a way of data augmentation
-__C.TRAIN.USE_FLIPPED = False
+__C.TRAIN.USE_FLIPPED = True
 
 # Deprecated (inside weights)
-__C.TRAIN.BBOX_INSIDE_WEIGHTS = np.ones(1,__C.TRAIN.NUM_REGRESSION)*1.0 # (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+__C.TRAIN.BBOX_INSIDE_WEIGHTS = np.ones((1,__C.TRAIN.NUM_REGRESSION))*1.0 # (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+
+
+__C.TRAIN.DEBUG =False  # True
 
 
 __C.TRAIN.SIGMA = 2.0
 # IOU >= thresh: positive example
 
-__C.TRAIN.LAMBDA = 1.0
+__C.TRAIN.LAMBDA = 0.6
 
 #__C.TRAIN.RPN_POSITIVE_OVERLAP = 0.7
 
@@ -74,19 +89,19 @@ __C.TRAIN.LAMBDA = 1.0
 # Alpha thershold between anchors and ground truth
 __C.TRAIN.POSITIVE_ALPHA = np.pi/4
 
-# Distance between central points
-__C.TRAIN.NEGATIVE_CEN_DIST = 0.88
+# The overlap to tell positive and negative samples
+__C.TRAIN.POSITIVE_THRESHOLD = 0.7
 
-__C.TRAIN.POSITIVE_CEN_DIST = 0.22
+__C.TRAIN.NEGATIVE_THRESHOLD = 0.2
 
 # Max number of foreground examples
 __C.TRAIN.RPN_FG_FRACTION = 0.5
 
 # Total number of examples
-__C.TRAIN.RPN_BATCHSIZE = 256
+__C.TRAIN.RPN_BATCHSIZE = 64
 
 # NMS threshold used on RPN proposals
-__C.TRAIN.RPN_NMS_THRESH = 0.7
+__C.TRAIN.RPN_THRESH = 0.7
 
 # Number of top scoring boxes to keep before apply NMS to RPN proposals
 __C.TRAIN.RPN_PRE_NMS_TOP_N = 1000
@@ -106,19 +121,22 @@ __C.TRAIN.RPN_BBOX_INSIDE_WEIGHTS = (1.0, 1.0, 1.0, 1.0)
 __C.TEST = edict()
 
 # number of 3D anchors
-__C.TEST.NUM_ANCHORS = 3
+__C.TEST.NUM_ANCHORS = 2
 
 # number of classification
 __C.TEST.NUM_CLASS = 2  ## background and vehicle, training from simple situation
 
-# number of regression parameters,  7 = x,y,z,l,w,h,theta
-__C.TEST.NUM_REGRESSION = 7
+# number of regression parameters,  7 = x,y,l,w
+__C.TEST.NUM_REGRESSION = 5
 
 # radius for grouping
-__C.TEST.Radius_1 = 1
-__C.TEST.Radius_2 = 1
-__C.TEST.Radius_3 = 1
-__C.TEST.Radius_4 = 1
+#__C.TEST.Radius_1 = 0.2
+#__C.TEST.Radius_2 = 0.6
+#__C.TEST.Radius_3 = 1.0
+#__C.TEST.Radius_4 = 1.8
+#__C.TEST.Radius_5 = 1.8
+#__C.TEST.Radius_6 = 2.4
+
 
 # 3D anchor size, the same size (l=3.9m, w=1.6m, h=1.7m) but with different
 # orientation alpha (pi = 0, pi/4, pi/2)
@@ -129,7 +147,7 @@ __C.TEST.Alpha = np.array([0,np.pi/2])
 __C.TEST.NMS = 0.3
 
 ## NMS threshold used on RPN proposals
-__C.TEST.RPN_NMS_THRESH = 0.7
+__C.TEST.RPN_THRESH = 0.7
 
 ## Number of top scoring boxes to keep before apply NMS to RPN proposals
 __C.TEST.RPN_PRE_NMS_TOP_N = 1000
