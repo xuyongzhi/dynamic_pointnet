0bece260b689124e55dd77ff129c7ad2328bce3b
benzlxs <xuesong.li@unsw.edu.au>
2018-05-26 17:29:33 +1000

    'halp in change'

--- utils_xyz/configs.py
+++ utils_xyz/configs.py
@@ -15,7 +15,7 @@ NETCONFIG['redundant_points_in_block'] = -777  # 'replicate' or a negative numbe
 _gsbb_config = '3D1_benz'
 #_gsbb_config = '2M2p'
 #_gsbb_config = '3D1_benz'
-_gsbb_config = '2S1'
+#_gsbb_config = '2S1'
 print('\n gsbb_config:%s \n-----------------------------------------------------'%(_gsbb_config))
 
 def get_gsbb_config( gsbb_config = _gsbb_config ):
--- utils_xyz/data_preprocess.py
+++ utils_xyz/data_preprocess.py
@@ -27,10 +27,10 @@ DATASETS = ['MATTERPORT', 'SCANNET', 'ETH', 'MODELNET40','KITTI']
 for ds in DATASETS:
     sys.path.append('%s/%s_util'%(BASE_DIR,ds))
 
-DATASET = 'SCANNET'
+#DATASET = 'SCANNET'
 #DATASET = 'ETH'
 #DATASET = 'MODELNET40'
-#DATASET = 'KITTI'
+DATASET = 'KITTI'
 DS_Meta = DatasetsMeta( DATASET )
 
 ORG_DATA_DIR = os.path.join(DATA_DIR, DATASET+'__H5F' )
