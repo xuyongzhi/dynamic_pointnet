fb9c9e08ac1b1674f8c3e684cf497086a83ecaad
xuyongzhi <buaaxyz@yeah.net>
2018-06-20 18:33:22 +1000

    ju

--- resnet3d/PBS/job.sh
+++ resnet3d/PBS/job.sh
@@ -24,9 +24,9 @@ learning_rate0=0.001
 num_gpus=2
 feed_data='xyzs'
 
-
-aug_types='none'
+aug_types='r-360_0_0'
 ./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data
 
-batch_size=32
+aug_types='r-0_0_360'
 ./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data
+
--- resnet3d/PBS/run_modelnet.sh
+++ resnet3d/PBS/run_modelnet.sh
@@ -3,9 +3,10 @@ model_flag='m'
 learning_rate0=0.01
 num_gpus=2
 feed_data='xyzs'
-rAnglesYXZ='360_0_0'
 
-aug_types='none'
 aug_types='r-360_0_0'
+./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data
+
+aug_types='r-0_360_0'
 ./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data
 
