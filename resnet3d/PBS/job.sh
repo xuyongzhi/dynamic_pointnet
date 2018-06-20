745eb520ca74915f2f34a868a1846c388cab7c59
xuyongzhi <buaaxyz@yeah.net>
2018-06-20 16:08:25 +1000

    use P100

--- resnet3d/PBS/job.sh
+++ resnet3d/PBS/job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -q gpu
+#PBS -q gpupascal
 #PBS -l walltime=20:00:00
 #PBS -l mem=30GB
 #PBS -l jobfs=0GB
-#PBS -l ngpus=4
+#PBS -l ngpus=2
 #PBS -l ncpus=12
 ## For licensed software, you have to specify it to get the job running. For unlicensed software, you should also specify it to help us analyse the software usage on our system.
 #PBS -l software=tensorflow/1.8-cudnn7.1-python2.7
@@ -20,9 +20,9 @@ module list
 
 batch_size=64
 model_flag='m'
-learning_rate0=0.01
+learning_rate0=0.001
 num_gpus=2
-feed_data='xyzg'
+feed_data='xyzs'
 
 
 aug='none'
