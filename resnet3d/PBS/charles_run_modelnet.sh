edab79b16820a57927b0c2520a3d5f3d37d10a31
xuyongzhi <buaaxyz@yeah.net>
2018-06-25 18:39:56 +1000

    update notes

--- resnet3d/PBS/charles_run_modelnet.sh
+++ resnet3d/PBS/charles_run_modelnet.sh
@@ -1,10 +1,11 @@
-batch_size=20
+batch_size=32
 model_flag='m'
 learning_rate0=0.001
 num_gpus=1
 feed_data='xyzrsg-nxnynz'
 drop_imo='0_0_5'
 aug_types='rpsfj-360_0_0'
+aug_types='psfj'
 #aug_types='N'
 
 ./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data $drop_imo
